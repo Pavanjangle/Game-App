ef8ddb12e28f517ff192be4f8e2613f6f2e9cb7a
Pavan P Jangale <pjangale88@gmail.com>
2024-01-15 15:54:48 +0530

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -2,10 +2,31 @@ import React, { useState } from "react";
 import "./Header.css";
 
 function Header() {
+
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+
+  const handleMenuIconClick = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  const handleLoginClick = () => {
+    setIsLoginModalOpen(true);
+  };
+
+  const handleCloseLoginModal = () => {
+    setIsLoginModalOpen(false);
+  };
+
+  const handleCloseSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+
   return (
     <div className="header-container">
       <div className="left-section">
-        <div className="menu-icon">&#9776;</div>
+        <div onClick={handleMenuIconClick} className="menu-icon">&#9776;</div>
 
         <div className="logo">
           <img
@@ -15,12 +36,35 @@ function Header() {
         </div>
       </div>
 
+      <div className={`sidebar ${isSidebarOpen ? "open" : ""}`}>
+        <div className="sidebar-content">
+          <button className="close-button" onClick={handleCloseSidebar}>
+            &times;
+          </button>
+          <img className="sidebar-img"
+            src="https://winbuzzbet.com/wp-content/uploads/2023/09/logo-2.png"
+            alt="Logo"
+          />
+          <h3>sidebar content here...</h3>
+        </div>
+      </div>
+
       <div className="right-section">
         <div className="buttons">
-          <button className="login">LOGIN</button>
+          <button className="login" onClick={handleLoginClick}>
+            LOGIN
+          </button>
           <button className="register">REGISTER</button>
         </div>
       </div>
+
+      {isLoginModalOpen && (
+        <div className="login-modal">
+          <h1>Work in progress..</h1>
+
+          <button onClick={handleCloseLoginModal}>CLOSE</button>
+        </div>
+      )}
     </div>
   );
 }
